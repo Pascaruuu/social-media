8d4071a3f1f120665ac94314685431c995f8f348
heng.vicheka <h.h.vicheka@gmail.com>
2026-04-09 17:17:26 +0700

    Reapply "Merge pull request #22 from Pascaruuu/21-fix-edit-delete-button"
    This reverts commit d1c73f484034a009734607c1a3ea173bc0792caa.

--- src/routes/components/post_card.svelte
+++ src/routes/components/post_card.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { Show } from 'svelte-clerk'
 	import ConfirmationModal from './confirmation_modal.svelte'
 	import default_profile from '$lib/assets/empty_profile.png'
 	import { helpers } from '$lib/helpers'
@@ -62,48 +63,51 @@
 			</div>
 		</div>
 
-		{#if can_manage}
-			<div class="post-card-actions">
-				<a
-					class="icon-action-btn"
-					href={get_edit_post_href()}
-					aria-label="Edit post"
-					title="Edit post"
-				>
-					<svg viewBox="0 0 24 24" aria-hidden="true">
-						<path d="M4 20h4.5L19 9.5 14.5 5 4 15.5V20Z" />
-						<path d="M12.5 7 17 11.5" />
-					</svg>
-				</a>
-
-				<form bind:this={delete_form} method="POST" action="?/delete_post" class="icon-action-form">
-					<input type="hidden" name="post_id" value={post.id} />
-					<input type="hidden" name="return_to" value={current_path} />
-					<button
-						class="icon-action-btn danger-action"
-						type="button"
-						aria-label="Delete post"
-						title="Delete post"
-						onclick={open_delete_modal}
+		<Show when="signed-in">
+			{#if can_manage}
+				<div class="post-card-actions">
+					<a
+						class="icon-action-btn"
+						href={get_edit_post_href()}
+						aria-label="Edit post"
+						title="Edit post"
 					>
 						<svg viewBox="0 0 24 24" aria-hidden="true">
-							<path d="M5 7h14" />
-							<path d="M9 7V5h6v2" />
-							<path d="M8 7l1 12h6l1-12" />
-							<path d="M10.5 10.5v6" />
-							<path d="M13.5 10.5v6" />
+							<path d="M4 20h4.5L19 9.5 14.5 5 4 15.5V20Z" />
+							<path d="M12.5 7 17 11.5" />
 						</svg>
-					</button>
-				</form>
-			</div>
-		{/if}
+					</a>
+
+					<form
+						bind:this={delete_form}
+						method="POST"
+						action="?/delete_post"
+						class="icon-action-form"
+					>
+						<input type="hidden" name="post_id" value={post.id} />
+						<input type="hidden" name="return_to" value={current_path} />
+						<button
+							class="icon-action-btn danger-action"
+							type="button"
+							aria-label="Delete post"
+							title="Delete post"
+							onclick={open_delete_modal}
+						>
+							<svg viewBox="0 0 24 24" aria-hidden="true">
+								<path d="M5 7h14" />
+								<path d="M9 7V5h6v2" />
+								<path d="M8 7l1 12h6l1-12" />
+								<path d="M10.5 10.5v6" />
+								<path d="M13.5 10.5v6" />
+							</svg>
+						</button>
+					</form>
+				</div>
+			{/if}
+		</Show>
 	</div>
 
 	<p class="post-content">{post.content}</p>
-	<!-- <div class="stats">
-		<span>Like 0</span>
-		<span>Views 0</span>
-	</div> -->
 </article>
 
 <ConfirmationModal
